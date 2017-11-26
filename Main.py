233252328728bbeaaa3c15019cc58c50c00bfa38
Rakesh Harish <32459979+HRakesh@users.noreply.github.com>
2017-11-26 13:46:24 -0500

    Add files via upload
    Rakesh Harish

--- Main.py
+++ Main.py
@@ -1,50 +1,166 @@
-from flask import Flask, render_template, request
+# Importing the necessary libraries for the analysis
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-app = Flask(__name__)
+from sklearn import tree
+#from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import classification_report
+from sklearn.cluster import KMeans
 
-@app.route('/')
-@app.route('/index.html')
-def index():
-    return render_template("index.html")
+#########################################
 
-@app.route('/tables.html')
-def tables():
-    return render_template("tables.html")
+# Exploring the data - EDA
+df = pd.read_csv('HR_comma_sep.csv', index_col=None)
 
-@app.route('/model1.html')
-def model1():
-    return render_template("model1.html")
+df = df.rename(columns={'sales' : 'department'})
 
-@app.route('/model2.html')
-def model2():
-    return render_template("model2.html")
+a=df.head()
+print(df.isnull().any())
+print(df.corr())
+print(df.head())
+print(df.describe())
+turnover_rate = (df.left.value_counts() / len(df))*100
+print("**************************************")
+print(turnover_rate)
+turnover_Summary = df.groupby('left')
+print(turnover_Summary.mean())
 
-@app.route('/model3.html')
-def model3():
-    return render_template("model3.html")
+# Correlation Matrix
+corr = df.corr()
+corr = (corr)
+ax= plt.axes()
+corr1=sns.heatmap(corr, xticklabels=corr.columns.values, yticklabels=corr.columns.values, ax=ax)
+ax.set_title('Correlation Matrix & Heatmap')
+print(corr)
 
-@app.route('/model4.html')
-def model4():
-    return render_template("model4.html")
+emp_population = df['satisfaction_level'][df['left'] == 0].mean()
+emp_turnover_satisfaction = df[df['left']==1]['satisfaction_level'].mean()
 
-@app.route('/predict.html')
-def predict():
-    return render_template("predict.html")
+print( 'The mean satisfaction for the employee population with no turnover is: ' + str(emp_population))
+print( 'The mean satisfaction for employees that had a turnover is: ' + str(emp_turnover_satisfaction))
 
-@app.route('/predict.html', methods=['POST'])
-def predicts():
-        satisfaction_level = request.form['satisfaction_level']
-        last_evaluation = request.form['last_evaluation']
-        number_of_projects = request.form['number_of_projects']
-        average_monthly_hours = request.form['average_monthly_hours']
-        years_at_company = request.form['years_at_company']
-        had_work_accident = request.form['had_work_accident']
-        promotion_in_last_5_years = request.form['promotion_in_last_5_years']
-        department = request.form['department']
-        salary = request.form['salary']
-        form = True
-        return render_template("predict.html", satisfaction_level=satisfaction_level, last_evaluation=last_evaluation, number_of_projects=number_of_projects, average_monthly_hours=average_monthly_hours, years_at_company=years_at_company, had_work_accident=had_work_accident, promotion_in_last_5_years=promotion_in_last_5_years, department=department, salary=salary, form=form)
+#sns.distplot(emp_population, kde=False, color="g",ax=axes[0]).set_title('mean satisfaction for the employee population with no turnover')
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+print("****************")
+
+#############
+
+f, ax = plt.subplots(figsize=(15, 5))
+sns.countplot(y="department", hue='left', data=df).set_title('Employee Department Turnover Distribution');
+
+f, ax = plt.subplots(figsize=(15, 4))
+sns.countplot(y="salary", hue='left', data=df).set_title('Employee Salary Turnover Distribution');
+
+##############
+
+# Set up the matplotlib figure
+f, axes = plt.subplots(ncols=3, figsize=(15, 6))
+
+# Graph Employee Satisfaction
+sns.distplot(df.satisfaction_level, kde=False, color="g", ax=axes[0]).set_title('Employee Satisfaction Distribution')
+axes[0].set_ylabel('Employee Count')
+
+# Graph Employee Evaluation
+sns.distplot(df.last_evaluation, kde=False, color="r", ax=axes[1]).set_title('Employee Evaluation Distribution')
+axes[1].set_ylabel('Employee Count')
+
+# Graph Employee Average Monthly Hours
+sns.distplot(df.average_montly_hours, kde=False, color="b", ax=axes[2]).set_title('Employee Average Monthly Hours Distribution')
+axes[2].set_ylabel('Employee Count')
+
+print("********************")
+
+###############################################
+
+# Graph and create 3 clusters of Employee Turnover
+kmeans = KMeans(n_clusters=3)
+kmeans.fit(df[df.left==1][["satisfaction_level","last_evaluation"]])
+
+colors = ['orange' if c == 0 else 'green' if c == 2 else 'red' for c in kmeans.labels_]
+
+fig = plt.figure(figsize=(10, 6))
+plt.scatter(x="satisfaction_level", y="last_evaluation", data=df[df.left==1],alpha=0.25,color=colors)
+plt.xlabel("satisfaction_level")
+plt.ylabel("last_evaluation")
+plt.scatter(x=kmeans.cluster_centers_[:, 0], y=kmeans.cluster_centers_[:,1], color="black", marker="X", s=100)
+plt.title("Clusters of Employee Turnover")
+
+###############################################
+
+plt.style.use('fivethirtyeight')
+plt.rcParams['figure.figsize'] = (12,6)
+
+# Convert these variables into categorical variables
+df["department"] = df["department"].astype('category').cat.codes
+df["salary"] = df["salary"].astype('category').cat.codes
+
+# Create train and test splits
+target_name = 'left'
+X = df.drop('left', axis=1)
+
+
+y=df[target_name]
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.15, random_state=123, stratify=y)
+
+dtree = tree.DecisionTreeClassifier(
+    #max_depth=3,
+    class_weight="balanced",
+    min_weight_fraction_leaf=0.01
+    )
+dtree = dtree.fit(X_train,y_train)
+print(dtree)
+## plot the importances ##
+importances = dtree.feature_importances_
+feat_names = df.drop(['left'],axis=1).columns
+
+
+indices = np.argsort(importances)[::-1]
+plt.figure(figsize=(12,6))
+plt.title("Decision Tree Classifier")
+#plt.bar(range(len(indices)), importances[indices], color='lightblue',  align="center")
+#plt.step(range(len(indices)), np.cumsum(importances[indices]), where='mid', label='Cumulative')
+#plt.xticks(range(len(indices)), feat_names[indices], rotation='vertical', color='Green', fontsize=10)
+#plt.xlim([-1, len(indices)])
+#plt.show()
+
+dtree = tree.DecisionTreeClassifier(
+    class_weight="balanced",
+    min_weight_fraction_leaf=0.01
+    )
+dtree = dtree.fit(X_train,y_train)
+print ("\n\n ---Decision Tree Model---")
+dt_roc_auc = roc_auc_score(y_test, dtree.predict(X_test))
+print ("Decision Tree AUC = %2.2f" % dt_roc_auc)
+print(classification_report(y_test, dtree.predict(X_test)))
+
+###########################################
+
+dtree = tree.DecisionTreeClassifier(
+    #max_depth=3,
+    class_weight="balanced",
+    min_weight_fraction_leaf=0.01
+    )
+dtree = dtree.fit(X_train,y_train)
+print ("\n\n ---Decision Tree Model---")
+dt_roc_auc = roc_auc_score(y_test, dtree.predict(X_test))
+print ("Decision Tree AUC = %2.2f" % dt_roc_auc)
+print(classification_report(y_test, dtree.predict(X_test)))
+
+# Plot Decision Tree ROC
+from sklearn.metrics import roc_curve
+dt_fpr, dt_tpr, dt_thresholds = roc_curve(y_test, dtree.predict_proba(X_test)[:,1])
+plt.plot(dt_fpr, dt_tpr, label='Decision Tree (accuracy = %0.2f)' % dt_roc_auc)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.legend(loc="lower right")
+
+###########################################
+
+plt.show()
